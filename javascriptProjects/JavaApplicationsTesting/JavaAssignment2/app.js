666b471f115a0519da5cdc6662814494f6227208
Alexandra Kmieciak <alexandra.kmieciak@gmail.com>
2020-09-24 09:45:47 -0400

    Reorganizing folders

---
+++ javascriptProjects/JavaApplicationsTesting/JavaAssignment2/app.js
@@ -0,0 +1,31 @@
+
+// Pass Line
+
+// In this game you roll the dice (called the come-out roll) and look at the total. 
+// If this total is equal to 7 or 11, then you win the amount of the bet. 
+// If the total is equal to 2, 3 or 12, then you lose the bet. 
+// Any other total of the dice becomes the "point", and you continue to roll the dice until you either 
+// (a) roll the point total again, in which case you win the amount of the bet, or 
+// (b) roll a total of 7, in which case you lose the amount of the bet. 
+// If the total of the dice is not 7 and not equal to the point, t
+// hen you roll again, and continue to roll, until either condition (a) or (b) is met. 
+// Once the point has been determined, it does not change for subsequent rolls of the dice.
+
+let amountOfMoney;
+let bet;
+
+let dice => {
+    let number1 = Math.random()
+}
+
+function passLine(){
+
+}
+
+function fieldBet(){
+
+}
+
+function anySeven(){
+
+}
